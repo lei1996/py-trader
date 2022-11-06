8ade0206a2ec46503ffc4906ff1cc8552db3d66c
lei1996 <617218165@qq.com>
2022-11-06 08:09:11 +0000

    update.

--- radical_monitor.sh
+++ radical_monitor.sh
@@ -1,4 +1,4 @@
 pm2 stop radical_monitor_hb
 echo '' > ~/.pm2/logs/radical-monitor-hb-out.log
 echo '' > ~/.pm2/logs/radical-monitor-hb-error.log
-pm2 restart radical_monitor_hb --cron-restart=0 --restart-delay=60000
+pm2 restart radical_monitor_hb --cron-restart=0 --restart-delay=10000
